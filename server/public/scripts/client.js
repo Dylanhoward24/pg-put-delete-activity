eea1e0fed80b1b681c721ff21ec4cc23be230589
Dylan Howard <dylanhoward0424@outlook.com>
2021-08-05 22:02:58 -0500

    finished functionality for isRead button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,7 @@ function addClickHandlers() {
 
   // TODO - Add code for edit & delete buttons
   $(document).on('click', '.deleteBtn', handleDelete);
+  $(document).on('click', '.updateBtn', handleUpdate);
 }
 
 function handleSubmit() {
@@ -39,6 +40,38 @@ function handleDelete() {
     console.log('DELETE /books error', err);
     alert('DELETE /books failed!');
   });
+}
+
+function handleUpdate() {
+    console.log('in handleUpdate');
+    // tr is table row, this will be the table
+    // row attached to the id of the delete button
+    let tr = $(this).parents('tr');
+    console.log('tr', tr);
+    // id is now being taken from the data of tr
+    // which we established above
+    let id = tr.data('id');
+    console.log('id', id);
+    // now we take the same as above, but do it
+    // for the isRead value
+    let isRead = tr.data('isRead');
+    console.log('isRead value is', isRead);
+
+    $.ajax({
+      method: 'PUT',
+      url: `/books/${id}`,
+      data: {
+        isRead: true
+      }
+    }).then((res) => {
+      console.log('PUT /books', res);
+
+      // refresh the data
+      refreshBooks();
+    }).catch((er) => {
+      console.log('PUT /books error', err);
+      alert('PUT /books failed!');
+    });
 }
 
 // adds a book to the database
@@ -78,9 +111,13 @@ function renderBooks(books) {
     let book = books[i];
     // For each book, append a new row to our table
     $('#bookShelf').append(`
-      <tr data-id="${book.id}">
+      <tr data-id="${book.id}" data-isRead="${book.isRead}">
         <td>${book.title}</td>
         <td>${book.author}</td>
+        <td>${book.isRead}</td>
+        <td>
+            <button class="updateBtn">Finished</button>
+        </td>
         <td>
             <button class="deleteBtn">Delete</button>
         </td>
